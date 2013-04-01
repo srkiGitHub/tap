524e158068241161fa7de59466ea670a4ea28073
avicentic <avicentic@gmail.com>
2013-04-01 18:16:55 +0200

    [NF] Add users implementation.

---
+++ tapcore/src/main/java/rs/vicko/security/UsersImpl.java
@@ -0,0 +1,18 @@
+package rs.vicko.security;
+
+public class UsersImpl implements Users
+{
+
+	public User getUser(int id)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUser(String username)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
