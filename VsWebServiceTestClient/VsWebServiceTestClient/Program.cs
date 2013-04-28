84fb3a11943d55ebc3ad647fca52a9685099de41
avicentic <avicentic@gmail.com>
2013-04-28 14:28:35 +0200

    [NF] add vs test client.

---
+++ VsWebServiceTestClient/VsWebServiceTestClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VsWebServiceTestClient.WsReference;
+
+namespace VsWebServiceTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TapWebService service = new TapWebServiceClient();
+            getUserbyNameResponse response = service.getUserbyName(new getUserbyNameRequest("sasa"));
+
+            Console.WriteLine("Full user name is " + response.@return.fullName);
+
+            Console.ReadLine();
+        }
+    }
+}
